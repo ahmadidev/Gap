1605ad9735c5c4481529ae31187c968e73f61d72
Ahmad Ahmadi <hakan648@yahoo.com>
2014-02-08 21:03:02 +0330

    Client' basic view controls
    simple view

--- Project/Gap.Win/Form1.Designer.cs
+++ Project/Gap.Win/Form1.Designer.cs
@@ -28,12 +28,104 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lbOnlineUsers = new System.Windows.Forms.ListBox();
+            this.lblOnlineUsers = new System.Windows.Forms.Label();
+            this.lbLogs = new System.Windows.Forms.ListBox();
+            this.lblLog = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(197, 12);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(48, 14);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(143, 20);
+            this.txtUsername.TabIndex = 0;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(12, 17);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(38, 13);
+            this.lblName.TabIndex = 2;
+            this.lblName.Text = "Name:";
+            // 
+            // lbOnlineUsers
+            // 
+            this.lbOnlineUsers.FormattingEnabled = true;
+            this.lbOnlineUsers.Location = new System.Drawing.Point(15, 76);
+            this.lbOnlineUsers.Name = "lbOnlineUsers";
+            this.lbOnlineUsers.Size = new System.Drawing.Size(257, 95);
+            this.lbOnlineUsers.TabIndex = 3;
+            // 
+            // lblOnlineUsers
+            // 
+            this.lblOnlineUsers.AutoSize = true;
+            this.lblOnlineUsers.Location = new System.Drawing.Point(12, 57);
+            this.lblOnlineUsers.Name = "lblOnlineUsers";
+            this.lblOnlineUsers.Size = new System.Drawing.Size(68, 13);
+            this.lblOnlineUsers.TabIndex = 4;
+            this.lblOnlineUsers.Text = "Online users:";
+            // 
+            // lbLogs
+            // 
+            this.lbLogs.FormattingEnabled = true;
+            this.lbLogs.Location = new System.Drawing.Point(15, 233);
+            this.lbLogs.Name = "lbLogs";
+            this.lbLogs.Size = new System.Drawing.Size(257, 95);
+            this.lbLogs.TabIndex = 5;
+            // 
+            // lblLog
+            // 
+            this.lblLog.AutoSize = true;
+            this.lblLog.Location = new System.Drawing.Point(12, 213);
+            this.lblLog.Name = "lblLog";
+            this.lblLog.Size = new System.Drawing.Size(28, 13);
+            this.lblLog.TabIndex = 6;
+            this.lblLog.Text = "Log:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 348);
+            this.Controls.Add(this.lblLog);
+            this.Controls.Add(this.lbLogs);
+            this.Controls.Add(this.lblOnlineUsers);
+            this.Controls.Add(this.lbOnlineUsers);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.btnConnect);
+            this.Name = "Form1";
+            this.Text = "Gap Client";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.ListBox lbOnlineUsers;
+        private System.Windows.Forms.Label lblOnlineUsers;
+        private System.Windows.Forms.ListBox lbLogs;
+        private System.Windows.Forms.Label lblLog;
+
     }
 }
 
--- Project/Gap.Win/Form1.cs
+++ Project/Gap.Win/Form1.cs
@@ -10,11 +10,19 @@ using System.Windows.Forms;
 
 namespace Gap.Win
 {
+    using System.Windows.Forms.VisualStyles;
+
     public partial class Form1 : Form
     {
+        private string clientName;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
         }
     }
 }
